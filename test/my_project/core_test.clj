7a951dbb67aed499048168290646ea92d3229972
Divna Popovic <divna_p@hotmail.com>
2016-01-28 14:22:17 +0100

    Organizing code, tried to rename packages but failed.

---
+++ test/my_project/core_test.clj
@@ -0,0 +1,12 @@
+(ns my-project.core-test
+  (:require [clojure.test :refer :all]
+            [my-project.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
+
+(deftest extract-test
+  (testing "extract merhod returns feeling from dictionaru"
+           (read-file)
+           (is (= "weak_negative" (extract "unable" "a")))))
